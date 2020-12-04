bcacf67585c7af4ed8e1cbaa8ff06c877c380347
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-04 14:48:05 +0900

    user/showアクションを定義

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,13 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+// function toggleNav() {
+//   var body = document.body;
+//   var hamburger = document.getElementById('js-hamburger');
+//   var blackBg = document.getElementById('js-black-bg');
 
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+//   hamburger.addEventListener('click', function() {
+//     body.classList.toggle('nav-open');
+//   });
+//   blackBg.addEventListener('click', function() {
+//     body.classList.remove('nav-open');
+//   });
+// }
+// toggleNav();
