5489ab6d70de0708badcaae2028d760a782fd83d
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-05 13:34:51 +0900

    user-edit実装
    nickname
    introduction
    genre
    を更新可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    redirect_to action: :show
   end
   
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :genre, :introduction, :image )
+  end
 end
