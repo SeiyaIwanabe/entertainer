6d1efe2df41ef7b70191f21eb273798d46a9c046
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-12 20:27:01 +0900

    routes.rb編集
    event/showアクション追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,13 @@
 class EventsController < ApplicationController
-  # before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @user = current_user
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 
   def new
@@ -13,7 +17,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      render 'create'
+      redirect_to root_path
     else
       render 'new'
     end
@@ -28,7 +32,7 @@ class EventsController < ApplicationController
   # end
 
   def event_params
-    params.require(:event).permit(:event_name, :datetime, :place, :reward, :detail).merge(recruiter_id: current_user.id)
+    params.require(:event).permit(:event_name, :datetime, :place, :prefecture, :reward, :detail).merge(recruiter_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_104031) do
+ActiveRecord::Schema.define(version: 2020_12_12_111139) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "event_name", null: false
     t.date "datetime", null: false
-    t.string "place", null: false
     t.integer "reward", null: false
     t.text "detail", null: false
     t.integer "recruiter_id"
