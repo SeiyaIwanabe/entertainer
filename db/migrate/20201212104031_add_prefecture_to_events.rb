d70a0a7fe2560bfed26f928d1d6d50406c58df67
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-12 19:42:29 +0900

    マイグレーションファイルを編集

--- db/migrate/20201212104031_add_prefecture_to_events.rb
+++ db/migrate/20201212104031_add_prefecture_to_events.rb
@@ -1,5 +1,5 @@
 class AddPrefectureToEvents < ActiveRecord::Migration[6.0]
   def change
-    add_column :events, :prefecture, :integer
+    add_column :events, :prefecture, :integer, null: false, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_044545) do
+ActiveRecord::Schema.define(version: 2020_12_12_104031) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "event_name", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_12_05_044545) do
     t.integer "applicant_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "prefecture", default: 0, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
