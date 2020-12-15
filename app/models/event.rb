4beec96e0e3ce989030bf6804f796578ed7819da
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-15 21:01:10 +0900

    修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,7 +18,7 @@ class Event < ApplicationRecord
   belongs_to :applicant, class_name: 'User', foreign_key: 'applicant_id', optional: true
 
   validates :event_name, presence: true
-  validates :event_name, length: { maximum: 20 }
+  validates :event_name, length: { maximum: 50 }
   validates :reward, presence: true
   validates :datetime, presence: true
   validates :prefecture, presence: true
