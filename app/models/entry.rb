9f7c831610cc0e0913ffe27c19621c218d5db2c4
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-12-17 15:48:01 +0900

    アソシエーション設定

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,5 @@ class Entry < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :event
 
-  validates_uniqueness_of :event_id, scope: user_id
+  validates_uniqueness_of :event_id, scope: :user_id
 end
