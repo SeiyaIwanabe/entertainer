9d337153e1ccf6e25f6629a8c2100927f537f23a
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-01-09 16:20:12 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-Carrierwave.configure do |config|
+CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:'AWS',
+    provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
