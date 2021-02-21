250766bcf08d827817e3f2244d8fee3ad23ca891
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-02-22 00:44:29 +0900

    Capistrano設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-# server '54.150.30.40', user: 'ec2-user', roles: %w{app db web}
+server '54.238.78.186', user: 'ec2-user', roles: %w{app db web}
