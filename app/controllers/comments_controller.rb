6fb640cf7be5426a4b771e62c852c7f34ff12df5
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-25 13:41:05 +0900

    コメント機能の非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
+  before_action :set_event, only: [:create, :destroy]
+  before_action :set_comment, only: :destroy
 
   def create
-    @comment = Comment.create(comment_params)
+    @comment = @event.comments.create(comment_params)
     if @comment.save
       redirect_back(fallback_location: root_path)
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     if @comment.destroy
       redirect_back(fallback_location: root_path)
@@ -17,6 +18,14 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, event_id: params[:event_id])
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
--- app/views/comments/create.json.jbuilder
+++
@@ -1,4 +0,0 @@
-# json.comment @comment.comment
-# json.user_id @comment.user.id
-# json.user.nickname @comment.user.nickname
-# json.user.icon.url @comment.user.icon.url
